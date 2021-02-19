21ada78925db074fd8aaba2d3bcef5ff67a8e74e
basakbilginoglu <basakbilginoglu@gmail.com>
2021-02-19 22:31:42 +0300

    Add files via upload

--- DataAccess/Concrete/InMemory/InMemoryCarDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -3,6 +3,7 @@ using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Concrete
@@ -53,7 +54,14 @@ namespace DataAccess.Concrete
            _cars.Remove(carToDelete);
         }
 
-        
-    
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Car Get(Expression<Func<Car, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
