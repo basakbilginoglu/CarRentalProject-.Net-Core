e88e408d1d4b2aa4aaf972d6c06b7c7e24924519
basakbilginoglu <basakbilginoglu@gmail.com>
2021-02-22 03:37:44 +0300

    CarDetailDto added to  Entities

--- Entities/Concrete/Brand.cs
+++ Entities/Concrete/Brand.cs
@@ -1,12 +1,14 @@
-﻿using System;
+﻿
+using Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Entities.Concrete
 {
-    public class Brand
+    public class Brand:IEntity
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        public int BrandId { get; set; }
+        public string BrandName { get; set; }
     }
 }
--- Entities/Concrete/Color.cs
+++ Entities/Concrete/Color.cs
@@ -1,12 +1,14 @@
-﻿using System;
+﻿
+using Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Entities.Concrete
 {
-    public class Color
+    public class Color:IEntity
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        public int ColorId { get; set; }
+        public string ColorName { get; set; }
     }
 }
